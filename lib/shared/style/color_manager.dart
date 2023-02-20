cd426b94b05240e7136e9445036f6d22949a2a48
Mahmoud Alawady <m.alawady00@gmail.com>
2023-02-20 23:28:10 +0200

    login and sign up UI

--- lib/shared/style/color_manager.dart
+++ lib/shared/style/color_manager.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
+
 class ColorManager {
   static Color primary = HexColor('FF4572');
   static Color white = HexColor('FFFFFF');
@@ -8,5 +9,8 @@ class ColorManager {
   static Color green = HexColor('50D890');
   static Color yellow = HexColor('FFC700');
   static Color black = HexColor('000000');
+  static Color lightPink = HexColor('EAEEF7');
+  static Color darkPink = HexColor('FF4572');
+
   // F6F7F9
 }
