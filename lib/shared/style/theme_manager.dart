e5cb096a45fb3ee56d5af336fbdbc2dbc2780528
Mahmoud Alawady <m.alawady00@gmail.com>
2023-02-23 23:47:07 +0200

    white status bar

--- lib/shared/style/theme_manager.dart
+++ lib/shared/style/theme_manager.dart
@@ -1,41 +1,42 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:qawarir/shared/style/styles_manager.dart';
 import 'package:qawarir/shared/style/values_manager.dart';
 import 'color_manager.dart';
 import 'fonts_manager.dart';
 
-ThemeData myTheme(){
+ThemeData myTheme() {
   return ThemeData(
-    // main color
-    scaffoldBackgroundColor: ColorManager.white,
-    primaryColor: ColorManager.primary,
-    // card theme
-    cardTheme: CardTheme(
-      color: ColorManager.grey2,
-    ),
+      // main color
+      scaffoldBackgroundColor: ColorManager.white,
+      primaryColor: ColorManager.primary,
+      // card theme
+      cardTheme: CardTheme(
+        color: ColorManager.grey2,
+      ),
 
-    // appBar Theme
-    appBarTheme: AppBarTheme(
-      color: ColorManager.white,
-      elevation: AppSize.s0,
-    ),
+      // appBar Theme
+      appBarTheme: AppBarTheme(
+        systemOverlayStyle:
+            SystemUiOverlayStyle(statusBarColor: ColorManager.white),
+        color: ColorManager.white,
+        elevation: AppSize.s0,
+      ),
       // ElevatedButtonTheme
       elevatedButtonTheme: ElevatedButtonThemeData(
         style: ElevatedButton.styleFrom(
           backgroundColor: ColorManager.white,
           textStyle: getMediumStyle(color: ColorManager.primary),
           shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(AppSize.s16)
-          ),
+              borderRadius: BorderRadius.circular(AppSize.s16)),
         ),
       ),
-    //input decoration Theme
-    inputDecorationTheme: InputDecorationTheme(
-      contentPadding: EdgeInsets.all(AppPadding.p8),
-      labelStyle: getRegularStyle(color: ColorManager.primary,fontSize: FontSize.s12),
-      fillColor: ColorManager.grey,
-      filled: true,
-    )
-  );
+      //input decoration Theme
+      inputDecorationTheme: InputDecorationTheme(
+        contentPadding: EdgeInsets.all(AppPadding.p8),
+        labelStyle: getRegularStyle(
+            color: ColorManager.primary, fontSize: FontSize.s12),
+        fillColor: ColorManager.grey,
+        filled: true,
+      ));
 }
