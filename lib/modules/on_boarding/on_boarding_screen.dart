af2505ad47023a1d2bb8962e3c9984a6ecb83dfd
Mahmoud Alawady <m.alawady00@gmail.com>
2023-02-25 19:42:40 +0200

    onboarding curve

--- lib/modules/on_boarding/on_boarding_screen.dart
+++ lib/modules/on_boarding/on_boarding_screen.dart
@@ -7,7 +7,7 @@ import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import '../../shared/style/color_manager.dart';
 import '../../shared/style/values_manager.dart';
 
-class BorderShow{
+class BorderShow {
   String image;
   String title;
   String body;
@@ -27,23 +27,20 @@ class OnBoardingScreen extends StatefulWidget {
 
 class _OnBoardingScreenState extends State<OnBoardingScreen> {
   var pageController = PageController();
-  bool isLast =false;
-  List<BorderShow>border =[
+  bool isLast = false;
+  List<BorderShow> border = [
     BorderShow(
         image: AssetManager.onBoarding1,
         title: AppStrings.onBoardingTitle1,
-        body: AppStrings.onBoardingSubTitle1
-    ),
+        body: AppStrings.onBoardingSubTitle1),
     BorderShow(
         image: AssetManager.onBoarding2,
         title: AppStrings.onBoardingTitle2,
-        body: AppStrings.onBoardingSubTitle2
-    ),
+        body: AppStrings.onBoardingSubTitle2),
     BorderShow(
         image: AssetManager.onBoarding3,
-        title:AppStrings.onBoardingTitle3,
-        body: AppStrings.onBoardingSubTitle3
-    ),
+        title: AppStrings.onBoardingTitle3,
+        body: AppStrings.onBoardingSubTitle3),
   ];
   @override
   Widget build(BuildContext context) {
@@ -55,56 +52,61 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
           children: [
             Expanded(
               child: PageView.builder(
-                itemBuilder: (context,index){
+                itemBuilder: (context, index) {
                   return Stack(
-                    children:   [
-                      Container(
-                        height: MediaQuery.of(context).size.height/4,
-                        width: MediaQuery.of(context).size.width,
-                        color: Colors.white,
-                        child:Padding(
-                          padding: const EdgeInsets.only(left: AppPadding.p18,top: AppPadding.p16),
-                          child: Align(
-                            alignment: Alignment.topLeft,
-                            child: Image(
-                              image: AssetImage(border[index].image),
-                              //  height: 500,
+                    children: [
+                      ClipPath(
+                        child: Container(
+                          height: MediaQuery.of(context).size.height * 0.32,
+                          width: MediaQuery.of(context).size.width,
+                          color: Colors.white,
+                          child: Padding(
+                            padding: const EdgeInsets.only(
+                                left: AppPadding.p18, top: AppPadding.p16),
+                            child: Align(
+                              alignment: Alignment.topLeft,
+                              child: Image(
+                                image: AssetImage(border[index].image),
+                                //  height: 500,
+                              ),
                             ),
                           ),
-                        ) ,
+                        ),
+                        clipper: CustomClipPath(),
                       ),
                       Positioned(
-                        top:MediaQuery.of(context).size.height/100 ,
-                        child:  Image(
+                        top: MediaQuery.of(context).size.height / 100,
+                        child: Image(
                           image: const AssetImage(AssetManager.onBoarding),
-                          height: MediaQuery.of(context).size.height/2.5,
+                          height: MediaQuery.of(context).size.height / 2.5,
                           width: MediaQuery.of(context).size.width,
+                          // to make image take full width
+                          fit: BoxFit.fill,
                           //  height: 500,
                         ),
                       ),
                       SizedBox(
-                        height:MediaQuery.of(context).size.height/4 ,
+                        height: MediaQuery.of(context).size.height / 4,
                       ),
                       Positioned(
-                        top:MediaQuery.of(context).size.height/2.3,
-                        left: MediaQuery.of(context).size.width/3.4,
-                        width: MediaQuery.of(context).size.width/1.5,
+                        top: MediaQuery.of(context).size.height / 2.3,
+                        left: MediaQuery.of(context).size.width / 3.4,
+                        width: MediaQuery.of(context).size.width / 1.5,
                         child: Text(
                           border[index].title,
                           style: getBoldStyle(color: ColorManager.white),
-
                         ),
                       ),
                       const SizedBox(
                         height: AppSize.s12,
                       ),
                       Positioned(
-                        top:MediaQuery.of(context).size.height/2,
-                        left: MediaQuery.of(context).size.width/6,
-                        width: MediaQuery.of(context).size.width/1.5,
+                        top: MediaQuery.of(context).size.height / 2,
+                        left: MediaQuery.of(context).size.width / 6,
+                        width: MediaQuery.of(context).size.width / 1.5,
                         child: Text(
                           border[index].body,
-                          style:getRegularStyle(color: ColorManager.white),
+                          style: getRegularStyle(color: ColorManager.white),
                           textAlign: TextAlign.center,
                         ),
                       ),
@@ -114,15 +116,14 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
                 itemCount: border.length,
                 controller: pageController,
                 physics: const BouncingScrollPhysics(),
-                onPageChanged: (int index){
-                  if(index == border.length-1) {
+                onPageChanged: (int index) {
+                  if (index == border.length - 1) {
                     setState(() {
-                      isLast =true;
+                      isLast = true;
                     });
-                  }
-                  else{
+                  } else {
                     setState(() {
-                      isLast =false;
+                      isLast = false;
                     });
                   }
                 },
@@ -131,7 +132,7 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
             SmoothPageIndicator(
               controller: pageController,
               count: border.length,
-              effect:  ExpandingDotsEffect(
+              effect: ExpandingDotsEffect(
                 dotColor: ColorManager.white.withOpacity(.2),
                 activeDotColor: ColorManager.white,
                 dotHeight: 8,
@@ -140,65 +141,82 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
                 spacing: 4,
               ),
             ),
-             SizedBox(
-              height: MediaQuery.of(context).size.height/8,
+            SizedBox(
+              height: MediaQuery.of(context).size.height / 8,
             ),
-            if(isLast!=true)
+            if (isLast != true)
               Container(
-              width:MediaQuery.of(context).size.width/1.5 ,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                ),
-                  onPressed: (){
-                    pageController.nextPage(duration: const Duration(
-                        milliseconds: 750
-                    ), curve: Curves.fastOutSlowIn
-                    );
-                  },
-                  child: Text(
-                      'Next',
-                    style: getMediumStyle(color: ColorManager.primary),
-                  )
-              ),
-            ),
-            if(isLast==true)
-              Container(
-                width:MediaQuery.of(context).size.width/1.5 ,
+                width: MediaQuery.of(context).size.width / 1.5,
                 child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                    ),
-                    onPressed: (){
-                      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>LoginScreen()));
+                    style: ElevatedButton.styleFrom(),
+                    onPressed: () {
+                      pageController.nextPage(
+                          duration: const Duration(milliseconds: 750),
+                          curve: Curves.fastOutSlowIn);
+                    },
+                    child: Text(
+                      'Next',
+                      style: getMediumStyle(color: ColorManager.primary),
+                    )),
+              ),
+            if (isLast == true)
+              Container(
+                width: MediaQuery.of(context).size.width / 1.5,
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(),
+                    onPressed: () {
+                      Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => LoginScreen()));
                     },
                     child: Text(
                       'Create an Account',
                       style: getMediumStyle(color: ColorManager.primary),
-                    )
-                ),
+                    )),
               ),
-            if(isLast!=true)
+            if (isLast != true)
               TextButton(
-                onPressed: (){
-                  Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(builder: (context)=>LoginScreen())
-                  );
+                onPressed: () {
+                  Navigator.pushReplacement(context,
+                      MaterialPageRoute(builder: (context) => LoginScreen()));
                 },
                 child: Text(
                   'skip',
                   style: getMediumStyle(color: ColorManager.white),
                 ),
-            ),
+              ),
             const SizedBox(
               height: AppSize.s20,
             ),
-            if(isLast==true)
+            if (isLast == true)
               const SizedBox(
-              height: AppSize.s50,
-            ),
+                height: AppSize.s50,
+              ),
           ],
         ),
       ),
     );
   }
+}
+
+class CustomClipPath extends CustomClipper<Path> {
+  var radius = 5.0;
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+
+    path.lineTo(0, size.height * 0.75);
+
+    path.quadraticBezierTo(size.width * 0.35, size.height * 1.2,
+        size.width * 0.8, size.height * 0.65);
+
+    path.lineTo(size.width, size.height * 0.9);
+
+    path.lineTo(size.width, 0);
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
 }
